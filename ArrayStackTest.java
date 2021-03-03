0718553bc85f30138b6e7918ff85139c875db5bf
Daniel Santamaria <santamaria.daniel316@gmail.com>
2021-03-03 12:31:19 -0800

    Tested Junit with test methods

--- ArrayStackTest.java
+++ ArrayStackTest.java
@@ -1,6 +1,24 @@
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class ArrayStackTest {
     public static void main(String[] args) {
+
+    }
+
+    public static int higher(int x, int y){
+        if (x > y)   
+            return x;
+        else 
+            return y;
+    }
+
+    @Test
+    public void Testhigher() {
+        assertEquals(25, higher(25, 25));
+        assertEquals(5, higher(3, 5));       
         
     }
-    
+
 }
