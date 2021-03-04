91f526175322dab962259a3fb4c46144472fbae9
Daniel Santamaria <santamaria.daniel316@gmail.com>
2021-03-03 20:57:20 -0800

    Set up for Junit testing

--- LinkedStackTest.java
+++ LinkedStackTest.java
@@ -1,11 +1,21 @@
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.Test;
+
 public class LinkedStackTest {
     public static void main(String[] args) {
-        LinkedStack<Character> expression = new LinkedStack<>();
-        System.out.println(expression.convertToPostfix("a*b/(c-a)+d*e")); 
+        //LinkedStack<Character> expression = new LinkedStack<>();
+        //System.out.println(expression.convertToPostfix("a*b/(c-a)+d*e")); 
     }
     
-/*     public static void testConvertToPostfix()
-    {
-
-    } */
+    public static String infixToPostfix() {
+        LinkedStack<Character> expression = new LinkedStack<>();
+        String Postfix = expression.convertToPostfix("a*b/(c-a)+d*e");
+        return Postfix;
+    }
+    
+    @Test
+    public void TestinfixToPostfix() {
+        assertEquals("ab*ca-/de*+", infixToPostfix());        
+    }
 }
